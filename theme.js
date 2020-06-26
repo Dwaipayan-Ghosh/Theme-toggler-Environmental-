43e5c19200e7ca9941de5c2533cf96df0712db5b
Dwaipayan Ghosh <Dwaipayanghosh2014@gmail.com>
2020-06-26 17:30:09 +0530

    finished toggler

---
+++ theme.js
@@ -0,0 +1,17 @@
+
+var body = document.getElementById("body");
+
+function good(){
+    console.log("shit")
+    body.style.backgroundImage = "url('good.jpg')"
+}
+
+function bad(){
+    console.log("shit")
+    body.style.backgroundImage =  "url('bad.jpg')"
+}
+
+function ugly(){
+    console.log("shit")
+    body.style.backgroundImage =  "url('ugly.jpg')"
+}
